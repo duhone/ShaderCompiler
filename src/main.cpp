e92f361fbd35eb15a4b25b36f00ad875d96c9391
duhone <duhone@outlook.com>
2020-05-12 12:11:27 -0500

    fix skip running bug

--- src/main.cpp
+++ src/main.cpp
@@ -106,10 +106,8 @@ int main(int argc, char** argv) {
 		app.exit(error);
 	}
 
-	if(fs::last_write_time(outputPath) > fs::last_write_time(vertPath)) {
-		return 0;    // nothing to do
-	}
-	if(fs::last_write_time(outputPath) > fs::last_write_time(fragPath)) {
+	if(fs::last_write_time(outputPath) > fs::last_write_time(vertPath) &&
+	   fs::last_write_time(outputPath) > fs::last_write_time(fragPath)) {
 		return 0;    // nothing to do
 	}
 
